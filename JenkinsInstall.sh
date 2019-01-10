c09a443b2f62103d698bf4e1643f92d5714a8f9e
Ben Leadbeater <40888832+nagatok1@users.noreply.github.com>
2019-01-10 09:44:40 +0000

    Update JenkinsInstall.sh

--- JenkinsInstall.sh
+++ JenkinsInstall.sh
@@ -2,6 +2,7 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo echo "deb https://pkg.jenkins.io/debian binary/" | sudo tee -a /etc/apt/sources.list
 sudo apt install openjdk-8-jre-headless
 sudo apt install openjdk-8-jdk
+sudo apt install maven
 sudo apt-get update
 sudo apt-get install jenkins
 
